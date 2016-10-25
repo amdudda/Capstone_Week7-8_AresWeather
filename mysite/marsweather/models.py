309b7decea7fa6b9aa8e34c273973ce818e51848
amdudda <tk0654wm@minneapolis.edu>
2016-10-24 19:53:00 -0500

    update database models to allow nulls

--- mysite/marsweather/models.py
+++ mysite/marsweather/models.py
@@ -12,18 +12,18 @@ class Weather(models.Model):
     #  https://docs.djangoproject.com/en/1.10/topics/db/models/#verbose-field-names
     terrestrial_date = models.DateField() # "2013-05-01",
     sol = models.IntegerField() #: 261,
-    ls = models.FloatField() #: 310.5,
-    min_temp = models.FloatField('low temp in Centigrade') #: -69.75,
-    min_temp_fahrenheit = models.FloatField('low temp in Fahrenheit') #: -93.55,
-    max_temp = models.FloatField('high temp in Centigrade') #": -4.48,
-    max_temp_fahrenheit = models.FloatField('high temp in Fahrenheit') #": 23.94,
-    pressure = models.FloatField() # : 868.05,
-    pressure_string = models.CharField("pressure descriptor", max_length=100) # "Higher",
-    abs_humidity = models.FloatField('absolute humidity') #: null,
-    wind_speed = models.FloatField()  #: 2.0
-    wind_direction = models.CharField # "--", this appears to almost always be null
-    atmo_opacity = models.CharField('atmospheric conditions',max_length=100) #: "Sunny",
-    season = models.CharField(max_length=10)#: "Month 11",
-    sunrise = models.DateTimeField() #": "2013-05-01T11:00:00Z",
-    sunset = models.DateTimeField() # "2013-05-01T22:00:00Z"
+    ls = models.FloatField(null=True) #: 310.5,
+    min_temp = models.FloatField('low temp in Centigrade',null=True) #: -69.75,
+    min_temp_fahrenheit = models.FloatField('low temp in Fahrenheit',null=True) #: -93.55,
+    max_temp = models.FloatField('high temp in Centigrade',null=True) #": -4.48,
+    max_temp_fahrenheit = models.FloatField('high temp in Fahrenheit',null=True) #": 23.94,
+    pressure = models.FloatField(null=True) # : 868.05,
+    pressure_string = models.CharField("pressure descriptor", max_length=100,null=True,blank=True) # "Higher",
+    abs_humidity = models.FloatField('absolute humidity',null=True) #: null,
+    wind_speed = models.FloatField(null=True)  #: 2.0
+    wind_direction = models.CharField(max_length=100,null=True) # "--", this appears to almost always be null
+    atmo_opacity = models.CharField('atmospheric conditions',max_length=100,null=True) #: "Sunny",
+    season = models.CharField(max_length=10,null=True,blank=True)#: "Month 11",
+    sunrise = models.DateTimeField(null=True) #": "2013-05-01T11:00:00Z",
+    sunset = models.DateTimeField(null=True) # "2013-05-01T22:00:00Z"
 # end Weather model
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'marsweather.apps.MarsweatherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
