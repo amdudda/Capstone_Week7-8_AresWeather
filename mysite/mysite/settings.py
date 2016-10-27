68402bbfac1d1c0228a678bf96ec960468bfbfd0
amdudda <tk0654wm@minneapolis.edu>
2016-10-27 12:52:27 -0500

    Registered MWdatabase as installed app.  Template now findable.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'MWdatabase.apps.MWdatabaseConfig',
     'marsweather.apps.MarsweatherConfig',
     'django.contrib.admin',
     'django.contrib.auth',
