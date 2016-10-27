2c54cdb5167bc7d87c2685cf78782efc6b5b3329
amdudda <tk0654wm@minneapolis.edu>
2016-10-27 10:09:24 -0500

    STILL not finding template.  Saving work and taking a break - may have to just manually enter a few records for now and circle back to autoload of data later.

--- mysite/MWdatabase/views.py
+++ mysite/MWdatabase/views.py
@@ -5,6 +5,6 @@ from django.template import loader
 # Create your views here.
 
 def MWindex(request):
-    return HttpResponse("Hello, Mars explorers!  This will be the Mars weather database loader page!")
-    # return render(request,'MWdatabase/index.html')
+    # return HttpResponse("Hello, Mars explorers!  This will be the Mars weather database loader page!")
+    return render(request,'MWdatabase/index.html')
     # The first line works but the second one doesn't -- why?
