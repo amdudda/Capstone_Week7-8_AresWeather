c26340696b5356d9fc2a6ddc1d42ace36cffdcfd
amdudda <tk0654wm@minneapolis.edu>
2016-10-27 13:38:28 -0500

    Start on automated data loader.

--- mysite/MWdatabase/static/MWdatabase/javascript/fetchWeatherData.js
+++ mysite/MWdatabase/static/MWdatabase/javascript/fetchWeatherData.js
@@ -2,6 +2,7 @@
  * Created by amdudda on 10/27/16.
  */
 
+// alert("loaded fetchWeatherData");
 // mars weather api at http://marsweather.ingenology.com/#get_started
 	function httpGetAsync(theUrl, callback){
 	    var xmlHttp = new XMLHttpRequest();
@@ -9,7 +10,9 @@
 		    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
 				callback(xmlHttp.responseText);
 			}
-			else { alert("current state is: " + xmlHttp.response); }
+			else {
+			    // alert("current state is: " + xmlHttp.response);
+			}
     }
 
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
@@ -19,7 +22,7 @@
 	function writeCurrentWeather(jsondata){
 		mydata = JSON.parse(jsondata);
 		weatherdata = mydata.report;
-		alert(weatherdata["terrestrial_date"]);
+		// alert(weatherdata["terrestrial_date"]);
 		info2print = "";
 		Object.keys(weatherdata).forEach(function(key) {
 			info2print += (key + ": " + weatherdata[key] + "\n");
@@ -30,10 +33,10 @@
 	// TODO: this only fetches the first page of results, we'll need to figure out how
 	// to page through results.  Maybe use a recursive function to simulate threading?
 	function writeMultiDayWeather(jsondata) {
-		//alert(jsondata);
+		// alert(jsondata);
 		mydata = JSON.parse(jsondata);
 		weatherdata = mydata.results;
-		//alert(weatherdata[0].sol);
+		// alert(weatherdata[0].sol);
 		info2print = "";
 		for (i=0; i<weatherdata.length; i++){
 			Object.keys(weatherdata[i]).forEach(function(key) {
