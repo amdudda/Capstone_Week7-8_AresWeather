0d323ecf2ca4db815bc7fd4b5f75c42edc941aa7
amdudda <tk0654wm@minneapolis.edu>
2016-10-28 07:16:00 -0500

    Show first day's data on index page.

--- mysite/marsweather/views.py
+++ mysite/marsweather/views.py
@@ -7,5 +7,8 @@ from .models import Weather
 def index(request):
     # return HttpResponse("Hello, Mars explorers!  This will e a Mars weather explorer app!")
     # get the first available sol's worth of data and send it to the home page as its context
-    weather = { 'weather': Weather.objects.order_by('-sol')[:1][0] }
-    return render(request, 'marsweather/index.html', weather)
+    w = Weather.objects.order_by('-sol')[:1][0]
+    ns = int(w.sol) + 1
+    print(ns)
+    context = { 'weather': w, 'next_sol': ns }
+    return render(request, 'marsweather/index.html', context)
