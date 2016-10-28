3acf8d2f5613300e50629a9d916eb8447ae26f78
amdudda <tk0654wm@minneapolis.edu>
2016-10-28 07:35:15 -0500

    Enable navigation by sol number.

--- mysite/marsweather/urls.py
+++ mysite/marsweather/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns =[
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<sol_id>[0-9]+)/$', views.sol , name='sol'),
 ]
 
 # regex101.com might help with future development.
--- mysite/marsweather/views.py
+++ mysite/marsweather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Weather
 
@@ -12,6 +12,15 @@ def index(request):
     # return HttpResponse("Hello, Mars explorers!  This will e a Mars weather explorer app!")
     # get the first available sol's worth of data and send it to the home page as its context
     w = Weather.objects.order_by('sol')[:1][0]
+    ns = int(w.sol) + 1
+    if (ns > last_sol): ns = None
+    ps = int(w.sol) - 1
+    if (ps < first_sol): ps = None
+    context = {'weather': w, 'next_sol': ns, 'prev_sol': ps}
+    return render(request, 'marsweather/index.html', context)
+
+def sol(request,sol_id):
+    w = get_object_or_404(Weather, sol=sol_id)
     ns = int(w.sol) + 1
     if (ns > last_sol): ns = None
     ps = int(w.sol) - 1
