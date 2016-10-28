7cfc6f6f814469657bc105b746d49ed329cbb2e6
amdudda <tk0654wm@minneapolis.edu>
2016-10-28 07:27:26 -0500

    Calculate and pass next and previous sol, with 'None' if they fall outside the range of available sols.

--- mysite/marsweather/views.py
+++ mysite/marsweather/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Weather
 
+# some useful globals
+first_sol = Weather.objects.order_by('sol')[:1][0].sol
+last_sol = Weather.objects.order_by('-sol')[:1][0].sol
+
 # Create your views here.
 
 def index(request):
     # return HttpResponse("Hello, Mars explorers!  This will e a Mars weather explorer app!")
     # get the first available sol's worth of data and send it to the home page as its context
-    w = Weather.objects.order_by('-sol')[:1][0]
+    w = Weather.objects.order_by('sol')[:1][0]
     ns = int(w.sol) + 1
-    print(ns)
-    context = { 'weather': w, 'next_sol': ns }
+    if (ns > last_sol): ns = None
+    ps = int(w.sol) - 1
+    if (ps < first_sol): ps = None
+    context = {'weather': w, 'next_sol': ns, 'prev_sol': ps}
     return render(request, 'marsweather/index.html', context)
