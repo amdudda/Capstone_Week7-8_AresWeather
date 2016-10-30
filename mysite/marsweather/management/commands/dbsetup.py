4c24cb892098b233a02adbc6890ca93e7f7e4c73
amdudda <tk0654wm@minneapolis.edu>
2016-10-30 08:41:24 -0500

    Create dbupdate utility.

--- mysite/marsweather/management/commands/dbsetup.py
+++ mysite/marsweather/management/commands/dbsetup.py
@@ -9,7 +9,7 @@ import requests  # documentation: http://docs.python-requests.org/en/master/user
 import json as JSON
 
 class Command(BaseCommand):
-    help = 'Fetches archival data from MAAS website.'
+    help = 'Fetches archival data from MAAS website by deleting and replacing existing data.'
 
     # def add_arguments(self, parser):
     #     parser.add_argument('poll_id', nargs='+', type=int)
@@ -47,59 +47,22 @@ class Command(BaseCommand):
         # end data upload
     # end handler
 
-    def load_api_data(self, url):
-
-        next_page = True  # set to true to get us through first page of data...
-        # counter used for debugging while loop
-        counter = 0
-        while next_page:  # and counter < 5:
-            r = requests.get(url).json()
-            next_page = r['next']
-            weatherdata = r['results']
-            for record in weatherdata:
-                # grab our records and store them in a weather object
-                w = Weather(
-                    terrestrial_date=record['terrestrial_date'],
-                    sol=record['sol'],
-                    ls=record['ls'],
-                    min_temp=record['min_temp'],
-                    min_temp_fahrenheit=record['min_temp_fahrenheit'],
-                    max_temp=record['max_temp'],
-                    max_temp_fahrenheit=record['max_temp_fahrenheit'],
-                    pressure=record['pressure'],
-                    pressure_string=record['pressure_string'],
-                    abs_humidity=record['abs_humidity'],
-                    wind_speed=record['wind_speed'],
-                    wind_direction=record['wind_direction'],
-                    atmo_opacity=record['atmo_opacity'],
-                    season=record['season'],
-                    sunrise=record['sunrise'],
-                    sunset=record['sunset']
-                )
-                # save the data to the database
-                w.save()
-            # end for record in weatherdata
-
-            # debugging; and print out to confirm data has been saved correctly
-            # print(Weather.objects.all());
-
-            # set url to next page in data
-            url = next_page
-            counter += 1
-            print("page " + str(counter) + " processed.")
-            # end while loop
-
-    # end load_api_data
-
-    def load_sample_data(self):
-        '''
-        This loads sample data to the database.  Legacy use only.
-        :return:
-        '''
-        devData = JSON.loads(
-            '{"count": 29, "next": "http://marsweather.ingenology.com/v1/archive/?page=2&terrestrial_date_end=2012-10-31&terrestrial_date_start=2012-10-01&format=json", "previous": null, "results": [{"terrestrial_date": "2012-10-31", "sol": 84, "ls": 198.6, "min_temp": -70.0, "min_temp_fahrenheit": -94.0, "max_temp": -0.5, "max_temp_fahrenheit": 31.1, "pressure": 8.04, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-30", "sol": 83, "ls": 198.0, "min_temp": -72.5, "min_temp_fahrenheit": -98.5, "max_temp": -0.5, "max_temp_fahrenheit": 31.1, "pressure": 8.0051, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-29", "sol": 82, "ls": 197.4, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -0.4, "max_temp_fahrenheit": 31.28, "pressure": 7.98, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-28", "sol": 81, "ls": 196.8, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.5, "max_temp_fahrenheit": 29.3, "pressure": 7.99, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-27", "sol": 80, "ls": 195.6, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.97, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-26", "sol": 79, "ls": 195.0, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -2.0, "max_temp_fahrenheit": 28.4, "pressure": 7.955, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-25", "sol": 78, "ls": 194.5, "min_temp": -71.0, "min_temp_fahrenheit": -95.8, "max_temp": 0.0, "max_temp_fahrenheit": null, "pressure": 7.94, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-24", "sol": 77, "ls": 193.9, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.93, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-23", "sol": 76, "ls": 193.9, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.91, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-22", "sol": 75, "ls": 193.0, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.91, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}]}')[
-            'results'];
-        for record in devData:
+def load_api_data(url):
+    '''
+    Gets data from an API via a user-supplied url
+    :param url: the url to use to request API data.
+    :return:
+    '''
+    next_page = True  # set to true to get us through first page of data...
+    total_records = 0
+    # counter used for debugging while loop and reporting pages of data
+    counter = 0
+    while next_page:  # and counter < 5:
+        r = requests.get(url).json()
+        total_records = r['count']
+        next_page = r['next']
+        weatherdata = r['results']
+        for record in weatherdata:
             # grab our records and store them in a weather object
             w = Weather(
                 terrestrial_date=record['terrestrial_date'],
@@ -121,7 +84,50 @@ class Command(BaseCommand):
             )
             # save the data to the database
             w.save()
-        # end for record in devData
+        # end for record in weatherdata
+
         # debugging; and print out to confirm data has been saved correctly
-        print(Weather.objects.all());
-    # end load_sample_data
+        # print(Weather.objects.all());
+
+        # set url to next page in data
+        url = next_page
+        counter += 1
+        print("page " + str(counter) + " processed.")
+        # end while loop
+    print('Database successfully updated with %d records of data.' % total_records)
+# end load_api_data
+
+def load_sample_data():
+    '''
+    This loads sample data to the database.  Legacy use only.
+    :return:
+    '''
+    devData = JSON.loads(
+        '{"count": 29, "next": "http://marsweather.ingenology.com/v1/archive/?page=2&terrestrial_date_end=2012-10-31&terrestrial_date_start=2012-10-01&format=json", "previous": null, "results": [{"terrestrial_date": "2012-10-31", "sol": 84, "ls": 198.6, "min_temp": -70.0, "min_temp_fahrenheit": -94.0, "max_temp": -0.5, "max_temp_fahrenheit": 31.1, "pressure": 8.04, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-30", "sol": 83, "ls": 198.0, "min_temp": -72.5, "min_temp_fahrenheit": -98.5, "max_temp": -0.5, "max_temp_fahrenheit": 31.1, "pressure": 8.0051, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-29", "sol": 82, "ls": 197.4, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -0.4, "max_temp_fahrenheit": 31.28, "pressure": 7.98, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-28", "sol": 81, "ls": 196.8, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.5, "max_temp_fahrenheit": 29.3, "pressure": 7.99, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-27", "sol": 80, "ls": 195.6, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.97, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-26", "sol": 79, "ls": 195.0, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -2.0, "max_temp_fahrenheit": 28.4, "pressure": 7.955, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-25", "sol": 78, "ls": 194.5, "min_temp": -71.0, "min_temp_fahrenheit": -95.8, "max_temp": 0.0, "max_temp_fahrenheit": null, "pressure": 7.94, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-24", "sol": 77, "ls": 193.9, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.93, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-23", "sol": 76, "ls": 193.9, "min_temp": -72.0, "min_temp_fahrenheit": -97.6, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.91, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}, {"terrestrial_date": "2012-10-22", "sol": 75, "ls": 193.0, "min_temp": -73.0, "min_temp_fahrenheit": -99.4, "max_temp": -1.0, "max_temp_fahrenheit": 30.2, "pressure": 7.91, "pressure_string": "Higher", "abs_humidity": null, "wind_speed": 2.0, "wind_direction": null, "atmo_opacity": null, "season": "Month 7", "sunrise": null, "sunset": null}]}')[
+        'results'];
+    for record in devData:
+        # grab our records and store them in a weather object
+        w = Weather(
+            terrestrial_date=record['terrestrial_date'],
+            sol=record['sol'],
+            ls=record['ls'],
+            min_temp=record['min_temp'],
+            min_temp_fahrenheit=record['min_temp_fahrenheit'],
+            max_temp=record['max_temp'],
+            max_temp_fahrenheit=record['max_temp_fahrenheit'],
+            pressure=record['pressure'],
+            pressure_string=record['pressure_string'],
+            abs_humidity=record['abs_humidity'],
+            wind_speed=record['wind_speed'],
+            wind_direction=record['wind_direction'],
+            atmo_opacity=record['atmo_opacity'],
+            season=record['season'],
+            sunrise=record['sunrise'],
+            sunset=record['sunset']
+        )
+        # save the data to the database
+        w.save()
+    # end for record in devData
+    # debugging; and print out to confirm data has been saved correctly
+    print(Weather.objects.all());
+# end load_sample_data
---
+++ mysite/marsweather/management/commands/dbupdate.py
@@ -0,0 +1,30 @@
+'''
+This takes data I gleaned from the API and saves it to the database.
+see https://docs.djangoproject.com/en/dev/howto/custom-management-commands/
+for how to set this up.  I left out _private.py since I don't seem to need it.
+'''
+from ...models import Weather
+from django.core.management.base import BaseCommand, CommandError
+from datetime import timedelta
+from .dbsetup import load_api_data
+
+class Command(BaseCommand):
+    help = 'Fetches new data from MAAS website by requesting data from after last available terrestrial date.'
+
+    def handle(self, *args, **options):
+        '''
+        this handles the dbupdate command by looking up the last available terrestrial_date of data, then passing
+        the next date to the url argument so it can request fresh data from the API.
+        :param args:
+        :param options:
+        :return:
+        '''
+        # calculate the appropriate date and concatenate our target url
+        update_url = 'http://marsweather.ingenology.com/v1/archive/?terrestrial_date_start='
+        start_date = Weather.objects.order_by('-terrestrial_date')[:1][0].terrestrial_date
+        start_date += timedelta(days=1)
+        update_url += str(start_date)
+        print("Requesting data using start date: %s." % str(start_date))
+
+        # request and store updated data
+        load_api_data(update_url)
